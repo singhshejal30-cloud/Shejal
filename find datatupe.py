f8510d6bb9a3a06d6b670824e83559a6d6f2d2f5
singhshejal30-cloud <singhshejal30@gmail.com>
2026-01-09 14:37:37 +0530

    Add files via upload

---
+++ find datatupe.py
@@ -0,0 +1,32 @@
+x = 20
+print(type(x))
+
+name = "shejal"
+print(type(name))
+
+y = 1.2
+print(type(y))
+
+# booleam.
+x = 10
+y = 5
+print(x > y)
+print(x < y)
+# find data type.
+age = 18
+print((age))
+
+phone = 7618957947
+print(type(phone))
+
+year = 2025
+print(type(year))
+
+x = 10
+y = "10"
+z = int(y)
+print(x + z)
+
+x = "shejal"
+y = "singh"
+print(x + y)
