e5f16e163317cbb915f8f299456b78b8302669d9
singhshejal30-cloud <singhshejal30@gmail.com>
2026-01-09 14:47:14 +0530

    Add files via upload

---
+++ data_str_type.py
@@ -0,0 +1,99 @@
+#list method
+
+# abc=[1,2,"shejal","shalu","riya"]
+# print(abc)
+
+
+# abc=[1,2,"shejal","riya"]
+# print(abc[-1])
+
+#slicing
+
+# abc= [1,2,'shejal','riya']
+# print(abc[0:3])
+# abc.append("AIPA")
+# print(abc)
+
+#insert method
+# abc= [1,2,'shejal','sneha','bittu']
+# abc.insert(1,'NSTI')
+# print(abc)
+
+#remove method
+
+# abc=[1,2,'shiya','riya','priya']
+# abc.remove('priya')
+# print(abc)
+
+#tuple method
+
+# name = ("shejal","riya","isha","arpit")
+# print(name)
+# print(name[-3])
+# print(name[0:3])
+
+# #type convertion
+# x= (1,2,3,4,5,)
+# y= list(x)
+# print(y)
+
+# #append method
+# x= (1,2,3,4,5,)
+# y= list(x)
+# y.append(6)
+# print(y)
+
+# # using all statments.
+# trade = ('aipa','csa','copa','dm','cosmo','em')
+# print(trade)
+# for i in trade:
+#     if i=="em":
+#         continue
+#     print(i)
+# print("\n em skipped")
+# print(trade[0:4])
+# print("end of conditions")
+
+# name= ("shejal","sree","riya","teena","aditi","aisha")
+# print(name)
+# for i in name:
+#     if i =="teena":
+#         continue
+#     if i =="aditi":
+#         break
+#     print(i)
+# print("teena is skipped")
+
+x= (1,2,3,4,5,6,7,8,9)
+print(max(x))
+print(min(x))
+print(sum(x))
+
+# average= sum(x)/len(x)
+# print(average)
+
+
+# x=[1,2,3,4,5]
+# z=x.pop(2)
+# print(z)
+# print(x)
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
