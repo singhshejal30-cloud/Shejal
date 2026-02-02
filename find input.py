1e452ec13ce849212c680922cbfcef45a517294f
singhshejal30-cloud <singhshejal30@gmail.com>
2026-02-02 09:14:17 +0530

    Add files via upload

---
+++ find input.py
@@ -0,0 +1,13 @@
+x = input("enter your name:")
+print("hello!", x )
+x = int(input("enter first number:"))
+y = int(input("enter second number:"))
+z = x + y
+print("sum of two numbers is:",z)
+
+#name.
+a = input("enter first name:")
+b = input("enter last name:")
+c = a+b
+print("full name is:" , c)
+ 
