8c149c00dc351a0a75f32ba60d364518beb6267e
singhshejal30-cloud <singhshejal30@gmail.com>
2026-02-02 09:15:39 +0530

    Add files via upload

---
+++ flow_control _statement.py
@@ -0,0 +1,83 @@
+# #flow control statements:
+# marks = int(input("enter your marks:"))
+# if marks>= 90:
+#     print("A+")
+# elif marks>= 80:
+#     print("B")
+# elif marks>= 70:
+#     print("C")
+# elif marks>= 60:
+#     print("D")
+# else:
+#     print("fail")
+
+# vehicle = input("enter vehicle:")
+# if vehicle in ("bmw, defender, scorpio"):
+#     print("four wheeler")
+# elif vehicle in ("spender, hero, shine"):
+#     print("two wheeler")
+# else:
+#     print("unknown")
+
+# # temerature checker.
+# temperature = int(input("enter the current temperature:"))
+# if temperature >= 30:
+#     print("it's a hot day!")
+# elif 20<= temperature <=30:
+#     print("nice weather!")
+# else:
+#     print("it's cold today!")
+    
+#  #student grading system.
+# marks = int(input("out of 100:"))
+# if marks>= 90:
+#     print("grade A")
+# elif marks>= 75:
+#     print("grade B")
+# elif marks>= 50:
+#     print("grade C")    
+# else:
+#     print("fail")
+
+# #traffic signal decision:
+# signal_color = input("enter the colors:")
+# if signal_color == ("red"):
+#     print("stop")
+# elif signal_color == (" yellow"):
+#     print("get ready")
+# elif signal_color == ("green"):
+#     print("go")
+# else:
+#     print("go now")
+
+#even or odd number:
+# number = int(input("enter number :"))
+# if(number % 2)== 0:
+#     print("even")
+# else:
+#     print("odd")
+
+# # movie ticket discount:
+# age = int(input("enter the age:")) 
+# if age <12:
+#     print("ticket price: 100")
+# elif 12<= age <=60:
+#     print("ticket price: 200")
+# elif age >60:
+#     print("ticket price:150")
+# else:
+#     print("invalid age")
+
+# # weather:
+# weather = (input("enter the weather:"))
+# if weather == "breezy":
+#     print("take jacket")
+# elif weather == "rainy":
+#     print("take umbrella")
+# elif weather == "sunny":
+#     print("go outside")
+# else:
+#     print("stay at home") 
+
+
+
