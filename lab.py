6906e41c1f944b9199c6875c83bca0e4530f8b50
Shejal Singh <singhshejal30@gmail.com>
2026-02-06 12:24:30 +0530

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,31 @@
+print(f"the result of {15} + {4} is: {15+4}")
+print(f"the result 0f {15} - {4} is: {15-4}")
+print(f"the result of {15} * {4} is: {15*4}")
+print(f"the result of {15} / {4} is: {15/4}")
+print(f"the result of {15} ** {4} is: {15**4}")
+print(f"the result of {15} // {4} is: {15//4}")
+
+print("the sum of 5 and 3 is", 5 + 3)
+print("the product of 4 and 7 is", 4 * 7)
+print("5", "+", "3", "=", 5 + 3, sep="")
+print("4", "*", "7", "=", 4 * 7, sep="*")
+print("5 + 3 =", end=" ")
+print(5 + 3)
+print("4 * 7 =", end=" ")
+print(4 * 7)
+
+x = 10
+print(x)
+print("hello, world!")
+print("welcome to python programming.")
+print("this is multi-line printing.")
+
+length = 10
+width = 5
+area = length * width
+print("the area of the rectangle is:", area )
+
+x = 10
+result = x * 2
+print("the result is:", result)
+
