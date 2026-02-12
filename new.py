2ad7b2c81edfd8f1c71a58b2d4947efa2a2588f8
Shejal Singh <singhshejal30@gmail.com>
2026-02-12 09:23:17 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,25 @@
+# print("the sum of 10 & 20 is:" , 10+20)
+# print("the substraction of 20 & 10 is:" , 20-10)
+# print(" the multiplication 0f 6 & 5 is:" , 6*5)
+# print("the division of 54 & 9 is:" , 54/9)
+# print(f" sum of {5} +{3} is: {5+3}")
+# print(f" substraction of {5} -{3} is: {5-3}")
+# print(f" multiplication of {5} * {3} is: {5*3}")
+# print(f"devision of {5} / {3} is: {5/3}")
+# x= 1
+# y= 2
+# c= x+y
+# print("the sum of " ,x, "and" ,y, "is" ,c)
+# print(f" the sum of {x} and {y} is {c}")
+# x = 5
+# y = 7
+# z = 10
+# a = x-y-z
+# b = x*y*z
+# c = x/y/z
+# print("subtraction of", x,y,z, "is", a )
+# print("multiplication of", x,y,z, "is", b)
+# print("division of", x,y,z, "is", c)
+# print(f"substration of {x}, {y}, {z}, is: {a}")
+# print(f"multiplication of {x}, {y}, {z}, is: {b}")
+# print(f"devision of {x}, {y}, {z}, is: {c}")
